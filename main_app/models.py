dd55e1abc063b896b2b164d63b367e11ed54e85c
laurhimms <lauriehimmel1@gmail.com>
2023-09-21 11:44:01 -0700

    create Feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 # Create your models here.
 class Bear(models.Model):
   species = models.CharField(max_length=100)
@@ -16,3 +23,18 @@ class Bear(models.Model):
   def get_absolute_url(self):
     return reverse('detail', kwargs={'bear_id': self.id})
   
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+     max_length=1,
+     choices=MEALS,
+     default=MEALS[0][0]
+    )
+  
+  bear = models.ForeignKey(
+     Bear,
+     on_delete=models.CASCADE
+    )
+
+  def __str__(self):
+     return f'{self.get_meal_display()} on {self.date}'
